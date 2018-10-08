2f8fa30efb4735d2729697b0f4897db3b430bf3d
sadiyaj <sadiyajiru@gmail.com>
2018-10-08 18:21:33 -0500

    working on survey

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-// Pull in required dependencies
-//recieve our info back in jason form
+const express = require('express');
+const path = require('path');
 var bodyParser = require('body-parser');
-var express = require('express');
-//path is built in node
-var path = require('path');
-//express makes routing easy using node js
-var app = express();
-
-
-
-// Configure the Express application
-//take whatever port is defined by Heroku or 8080 for local
-var PORT = process.env.PORT || 8080;
-// Add middleware for parsing incoming request bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-
-// Expose the public directory to access CSS files
+app.use(express.json());
 app.use(express.static(path.join(__dirname, './app/public')));
-
 
 
 // Add the application routes
