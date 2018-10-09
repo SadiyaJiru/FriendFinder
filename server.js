932fb934a450222df84167f5d8e75d2f5b91c92f
sadiyaj <sadiyajiru@gmail.com>
2018-10-08 21:19:53 -0500

    fixing server

--- server.js
+++ server.js
@@ -3,26 +3,15 @@ const path = require('path');
 var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
-// app.use(express.urlencoded({extended: false }));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(bodyParser.text());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/app/public')));
+app.use(express.static(path.join(__dirname, './app/public')));
 
 
 // Add the application routes
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
-// require('./app/routing/apiRoutes.js')(app);
-// require('./app/routing/htmlRoutes.js')(app);
-// Start listening on PORT
-
-
-
-
-//If the form was posted correctly page will show new frend without refresh 
-// res.redirect("/");
 
 app.listen(PORT, function() {
     //we will know when the server started to run
