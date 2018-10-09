773af8ca020d9b4af72a2a4cf173685bfc43d3a3
sadiyaj <sadiyajiru@gmail.com>
2018-10-08 20:43:22 -0500

    fixing server error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './app/public')));
