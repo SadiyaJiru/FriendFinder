6befa909a6042905fbaf94132d3c36deb174a837
sadiyaj <sadiyajiru@gmail.com>
2018-10-09 17:04:08 -0500

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -29,7 +29,7 @@ var friends = [{
     scores: ['1', '2', '4', '5','4','2','3','3','5','5']
  },
  {
-    name: "Jon Heder",
+    name: "Jennifer Aniston",
     photo: "https://www.telegraph.co.uk/content/dam/beauty/2016/04/27/Jennifer-Aniston_trans_NvBQzQNjv4BqkX3onuOCNa0uuigJZQxtEBOJpD29tp1fMw9A0Cp6c54.jpg?imwidth=450",
     scores: ['5', '5', '3', '5','4','1','3','1','5','5']
  },
