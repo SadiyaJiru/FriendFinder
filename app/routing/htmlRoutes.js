320e44ee5e453628e954cf6c62e4bc40dcf2b558
sadiyaj <sadiyajiru@gmail.com>
2018-10-09 00:28:50 -0500

    apiroute

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,8 +8,7 @@ var path = require('path');
 module.exports = function(app) {
 	// console.log('___ENTER htmlRoutes.js___');
 
-	// A default, catch-all route that leads to `home.html` which displays the home page.
-	app.get('/', function(req, res) {
+	app.get('/home', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
  //display the survey page.
