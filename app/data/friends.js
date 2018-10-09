c25221e389643a2389be12c04f4aae003ccbbe10
sadiyaj <sadiyajiru@gmail.com>
2018-10-09 15:12:37 -0500

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,11 +4,35 @@ var friends = [{
     scores: ['1', '5', '4', '1','4','4','2','3','3','2']
 },
 {
-    name: "Beyonce",
+    name: "Beyonce Knowls",
     photo: "https://peopledotcom.files.wordpress.com/2017/10/beyonce-selena-gallery.jpeg",
     scores: ['4', '2', '1', '5','4','4','1','3','2','2']
-}
-
+},
+{
+   name: "Sadiya J",
+   photo: "https://yt3.ggpht.com/a-/ACSszfHaBmQkAicppgZ49wAAinyW2-FcrPFRsg2g9A=s900-mo-c-c0xffffffff-rj-k-no",
+   scores: ['5', '2', '1', '3','4','2','2','5','1','3']
+},
+{
+    name: "George Costanza",
+    photo: "https://vignette.wikia.nocookie.net/seinfeld/images/7/76/George-costanza.jpg/revision/latest?cb=20110406222711",
+    scores: ['5', '2', '1', '3','4','2','2','3','1','5']
+ },
+ {
+    name: "Will Smith",
+    photo: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzQzMTY2NDc4/will-smith-9542165-1-402.jpg",
+    scores: ['4', '5', '2', '3','4','2','1','3','4','5']
+ },
+ {
+    name: "Jon Heder",
+    photo: "https://pbs.twimg.com/profile_images/976941155370795014/ZzdOhH9C_400x400.jpg",
+    scores: ['1', '2', '4', '5','4','2','3','3','5','5']
+ },
+ {
+    name: "Jon Heder",
+    photo: "https://www.telegraph.co.uk/content/dam/beauty/2016/04/27/Jennifer-Aniston_trans_NvBQzQNjv4BqkX3onuOCNa0uuigJZQxtEBOJpD29tp1fMw9A0Cp6c54.jpg?imwidth=450",
+    scores: ['5', '5', '3', '5','4','1','3','1','5','5']
+ },
 ];
 
 module.exports = friends;
